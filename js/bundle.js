d5a53437f7c6bd7eb38de10228d218222a15cf70
josh <joshsmith2@gmail.com>
2018-06-30 13:39:29 +0100

    "Committing before refactoring word generation"

--- js/bundle.js
+++ js/bundle.js
@@ -6,26 +6,50 @@ window.latinum= {};
 
 $(function(){
     var generate_message = function(){
-        words = randomWords({exactly: 2});
-        return "You're really " + words[0] + "ing my " + words[1] + "."
+        var words = randomWords({exactly: 2});
+        var vowelish = ['a','e','i','o','u','y','s'];
+        var sentence = "";
+        if (words[1].substr(-1,1) == "s"){
+            sentence = "You're all a bunch of " + words[0] + ' ' + words[1] + '.';
+        } else if (vowelish.includes(words[0].substr(-1,1)))  {
+            sentence = "Latitude is a place for " + words[0] + " " + words[1] + ".";
+        } else {
+            var suffix = "ing";
+            if (words[0].substr(-3,3) == 'ing'){
+                suffix = ""
+            }
+            sentence = "You're really " + words[0] + suffix + " my " + words[1] + ".";
+        }
+        return sentence;
+    };
+
+
+    var count_trailing_zeroes = function(hash){
+        var zeroes = 0;
+        hash =  $.trim(hash);
+        for (var i=1; i <= hash.length; i++){
+            if (hash.substr(-i,1) !== '0'){
+                zeroes = i-1;
+                return zeroes
+            }
+        }
     };
 
     latinum.generate_message = generate_message;
-
+    latinum.count_trailing_zeroes = count_trailing_zeroes;
 });
 
 $(document).ready(function() {
 
     var msg;
     var human_message;
-
-    human_message = latinum.generate_message();
-
-    msg = sha256.x2(human_message);
-
-    $('#human').text(human_message);
-    $("#message").text(msg);
-
+    $(document).keypress(function(){
+        human_message = latinum.generate_message();
+        msg = sha256.x2(human_message);
+        $('#human').text(human_message);
+        $("#message").text(msg);
+        $("#ze-2").text(latinum.count_trailing_zeroes(msg));
+    });
 });
 
 },{"random-words":4,"sha256":5}],2:[function(require,module,exports){
