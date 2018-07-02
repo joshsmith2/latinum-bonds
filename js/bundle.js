c5132c2a71ace842225d99d8a5b47da3df1623d2
josh <joshsmith2@gmail.com>
2018-07-02 13:33:48 +0100

    Band names now appearing

--- js/bundle.js
+++ js/bundle.js
@@ -18,8 +18,19 @@ $(function(){
        return get_random(adjectives, 5176);
     };
 
-    var noun = function(){
-        return get_random(nouns, 2273);
+    var noun = function(min_length){
+        out_noun = '';
+        if (min_length != null) {
+            while (out_noun = '') {
+                candidate = get_random(nouns, 2273);
+                if (candidate.length > 8) {
+                    out_noun = candidate;
+                }
+            }
+        } else {
+            out_noun = get_random(nouns, 2273);
+        }
+        return out_noun;
     };
 
     var a_noun = function(){
@@ -31,12 +42,30 @@ $(function(){
     };
 
     var generate_message = function(){
-        var sentences = ["Latitude is " + an_adj() + " " + noun() + ".",
-                         "You are " + an_adj() + " " + adj() + " " + noun() + ".",
-                         "Performing on the main stage: " + capital(adj()) + " " + capital(noun()) + ".",
-                         "This " + noun() + " tastes oddly like " + an_adj() + " " + noun() + ".",
-                         "We'll make " + a_noun() + " out of you yet.",
-                         "Are we human? Or are we " + noun() + "?"];
+        var normal_sentences = [
+                "Latitude is " + an_adj() + " " + noun() + ".",
+                "You are " + an_adj() + " " + adj() + " " + noun() + ".",
+                "Performing on the main stage: " + capital(adj()) + " " + capital(noun()) + ".",
+                "This " + noun() + " tastes oddly like " + an_adj() + " " + noun() + ".",
+                "We'll make " + a_noun() + " out of you yet.",
+                "Are we Human? Or are we " + capital(noun()) + "?"
+        ];
+
+        var band_names = [
+            "Noah and the " + capital(noun()) + ".",
+            "alt-" + noun() + ".",
+            "Aphex " + capital(noun()) + ".",
+            "Hedwig and the " + capital(adj()) + " " + capital(noun()),
+            capital(adj()) + " " + capital(noun()) + ".",
+            capital(an_adj()) + " " + capital(noun()) + ".",
+            noun().toUpperCase() + "!",
+            capital(adj()) + " Lewis and the " + capital(noun()) + ".",
+            capital(noun()) + "-Yards.",
+            noun().replace(/[aeiou]/g, '')
+        ];
+
+        var sentences = band_names;
+
         return get_random(sentences, sentences.length);
     };
 
@@ -61,12 +90,36 @@ $(document).ready(function() {
 
     var msg;
     var human_message;
+    var best_message = '';
+    var best_score = 0;
+    var best_head = "";
+    var best_tail = "";
+
     $(document).keypress(function(){
+
         human_message = latinum.generate_message();
+
         msg = sha256.x2(human_message);
-        $('#human').text(human_message);
-        $("#message").text(msg);
-        $("#ze-2").text(latinum.count_trailing_zeroes(msg));
+        score = latinum.count_trailing_zeroes(msg);
+        var head = msg.substr(0,64-score);
+        var tail = msg.substr(-score,score);
+
+        if (score >= best_score) {
+            best_score = score;
+            best_message = human_message;
+            best_head = head;
+            best_tail = tail;
+        }
+
+        $('#latest-message').text(human_message);
+        $('#latest-hash .head').text(head);
+        $("#latest-hash .tail").text(tail);
+        $(".score#last").text(score);
+
+        $('#best-message').text(best_message);
+        $('#best-hash .head').text(best_head);
+        $("#best-hash .tail").text(best_tail);
+        $(".score#best").text(best_score);
     });
 });
 
